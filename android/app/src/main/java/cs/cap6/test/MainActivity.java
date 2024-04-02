b91a8dd0adf4e750d69a71bac959e8248853e460
damian.ionic <damian@ionic.io>
2024-04-02 07:18:57 -0700

    initial commit

---
+++ android/app/src/main/java/cs/cap6/test/MainActivity.java
@@ -0,0 +1,5 @@
+package cs.cap6.test;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cs.cap6.test',
+  appName: 'cs-cap6-test',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
